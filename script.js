c3bc0934bae896f2b89867718e95b5f12429f6f5
ozgurdemirten <ozgur1996c@gmail.com>
2023-06-15 22:25:15 +0300

    js sonraki commit

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 const cartBtn=document.querySelector(".cart-btn");
 const clearCartBtn=document.querySelector(".cart-clear");
 const cartItems=document.querySelector(".cart-items");
-const cartTotal=document.querySelector(".cart-value");
+const cartTotal=document.querySelector(".total-value");
 const cartContent=document.querySelector(".cart-list");
 const productsDOM=document.querySelector("#products-dom");
 
 
 let buttonsDOM=[];
+let cart=[];
 class Products{
     async getProducts()
 {
@@ -26,7 +27,7 @@ class Products{
 
 class UI{
     displayProducts(products){
-        console.log(products);
+       // console.log(products);
         let result="";
         products.forEach(item => {
             result+=` <div class="col-lg-4 col-md-6">
@@ -45,19 +46,68 @@ class UI{
         </div> `
         });
         productsDOM.innerHTML=result;
-        console.log(result);
     }
     getBagButtons(){
         const buttons=[...document.querySelectorAll(".btn-add-to-cart")];
         buttonsDOM=buttons;
         buttons.forEach(button=>{
             let id =button.dataset.id;
-            console.log(id);
+            let inCart=cart.find(item=>item.id===id);
+            if(inCart){
+                button.setAttribute("disabled","disabled");
+                button.opacity=".3";
+            }
+            else{
+                button.addEventListener("click",event=>{
+                    event.target.disabled=true;
+                    event.target.style.opacity=".3";
+                    //* get product from products
+                    let cartItem={...Storage.getProduct(id),amount:1};
+                    //* add product to the cart
+                    cart = [...cart, cartItem];
+                    //*sae cart in local storage
+                    Storage.saveCart(cart);
+                    //* save cart values
+                    this.saveCartValues(cart);
+                    //* display cart item
+                    this.addCardItem(cartItem);
+                })
+            }
         })
+    }
+    saveCartValues(cart){
+        let tempTotal=0;
+        let itemsTotal=0;
+        cart.map(item=>{
+            tempTotal+= item.price*item.amount;
+            itemsTotal+=item.amount;
+        });
+        console.log(cartTotal);
+        cartTotal.innerText=parseFloat(tempTotal.toFixed(2));
+        cartItems.innerText=itemsTotal;
+    }
+    addCardItem(item){
+        const li =document.createElement("li");
+        li.classList.add("cart-list-item");
+        li.innerHTML=`
+        
+        `
     }
 }
 
 class Storage{
+    static saveProducts(products){
+        localStorage.setItem("products",JSON.stringify(products));
+    }
+    static getProduct(id){
+        let products=JSON.parse(localStorage.getItem("products"));
+         return products.find(product=>product.id===id);
+
+    }
+    static saveCart(cart){
+        localStorage.setItem("cart",JSON.stringify(cart));
+    }
+
 
 }
 document.addEventListener("DOMContentLoaded",()=>{
@@ -65,6 +115,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     const products=new Products();
     products.getProducts().then(products=>{
         ui.displayProducts(products);
+        Storage.saveProducts(products);
     }).then(()=>{
         ui.getBagButtons();
     })
